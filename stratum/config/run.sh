28c2f8341a4e969c6e3db0f2dff67592edc2a745
Caesar Salad <29797621+Revasz@users.noreply.github.com>
2018-03-06 20:46:15 +0100

    Update run.sh

--- stratum/config/run.sh
+++ stratum/config/run.sh
@@ -3,10 +3,13 @@
 ulimit -n 10240
 ulimit -u 10240
 
+#echo "$1 stratum started" | mail -s "$1 stratum started" your@email.com # change email address to valid one
+
 cd /var/stratum
 while [ -e config/${1}.conf ]; do
 	gzip -f config/${1}.log
         ./stratum config/$1
+	#echo "$1 stratum crashed/restarted" | mail -s "$1 stratum crashed/restarted" your@email.com # change email address to valid one
 	sleep 1
 done
 exec bash
